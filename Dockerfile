759bcc78819b0606b1342495d4f225db272e8aee
Alex Gollion <alex.gollion@gmail.com>
2025-07-02 11:22:58 +0200

    ajout des fixtures

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 FROM php:8.2-apache
 
 RUN apt-get update \
- && apt-get install -y zlib1g-dev g++ git libicu-dev zip libzip-dev zip \
+ && apt-get install -y zlib1g-dev g++ git libicu-dev zip libzip-dev zip libpq-dev \
  && docker-php-ext-install intl opcache pdo pdo_mysql \
  && pecl install apcu \
  && docker-php-ext-enable apcu \
  && docker-php-ext-configure zip \
  && docker-php-ext-install zip \
  && pecl install xdebug \
- && docker-php-ext-enable xdebug
+ && docker-php-ext-enable xdebug \
+ && docker-php-ext-install pgsql pdo_pgsql
 
 RUN a2enmod rewrite
 
--- src/Doctrine/DataFixtures/VideoGameFixtures.php
+++ src/Doctrine/DataFixtures/VideoGameFixtures.php
@@ -4,6 +4,8 @@ namespace App\Doctrine\DataFixtures;
 
 use App\Model\Entity\User;
 use App\Model\Entity\VideoGame;
+use App\Model\Entity\Review;
+use App\Model\Entity\Tag;
 use App\Rating\CalculateAverageRating;
 use App\Rating\CountRatingsPerValue;
 use DateTimeImmutable;
@@ -25,6 +27,8 @@ final class VideoGameFixtures extends Fixture implements DependentFixtureInterfa
 
     public function load(ObjectManager $manager): void
     {
+        $tags = $manager->getRepository(Tag::class)->findAll();
+
         $users = $manager->getRepository(User::class)->findAll();
 
         $videoGames = array_fill_callback(0, 50, fn (int $index): VideoGame => (new VideoGame)
@@ -38,17 +42,39 @@ final class VideoGameFixtures extends Fixture implements DependentFixtureInterfa
         );
 
         // TODO : Ajouter les tags aux vidéos
+        array_walk($videoGames, static function (VideoGame $videoGame) use ($tags): void {
+            for ($i = 0; $i < 5; $i++) {
+                $videoGame->getTags()->add($tags[rand(0, count($tags) - 1)]);
+            }
+        });
 
         array_walk($videoGames, [$manager, 'persist']);
-
-        $manager->flush();
-
+        
         // TODO : Ajouter des reviews aux vidéos
+        array_walk($videoGames, function (VideoGame $videoGame) use ($users, $manager): void {
+            for ($i = 0; $i < rand(0, 5); $i++) {
+                $review = (new Review)
+                ->setUser($users[rand(0, count($users) - 1)])
+                ->setVideoGame($videoGame)
+                ->setRating(rand(1, 5))
+                ->setComment($this->faker->paragraphs(6, true));
+                
+                $videoGame->getReviews()->add($review);
+                
+                $manager->persist($review);
 
+                $this->calculateAverageRating->calculateAverage($videoGame);
+                $this->countRatingsPerValue->countRatingsPerValue($videoGame);
+                
+            }
+        });
+        
+        $manager->flush();
     }
 
     public function getDependencies(): array
     {
-        return [UserFixtures::class];
+        return [UserFixtures::class, TagFixtures::class];
     }
+
 }
